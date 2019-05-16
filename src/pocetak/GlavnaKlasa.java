e4b19bb20475e11e5d8bbc8c3ef9e617c8320ceb
oik <oik@LAPTOP-BNEH5OLG>
2019-05-16 19:20:08 +0200

    prvi commit

---
+++ src/pocetak/GlavnaKlasa.java
@@ -0,0 +1,47 @@
+package pocetak;
+
+import java.util.Scanner;
+
+public class GlavnaKlasa {
+
+	public static void main(String[] args) {
+
+		Scanner scanner =  new Scanner(System.in);
+		
+		System.out.println("Unesite id.");
+		int id = Integer.parseInt(scanner.nextLine());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*String ime = MetodeJdbc.dajImeEntuzijaste(id);
+		System.out.println(ime);
+		System.out.println("Unesite ime: ");
+		String ime = scanner.nextLine();
+		
+		System.out.println("Unesite prezime: ");
+		String prezime = scanner.nextLine();
+		
+		System.out.println("Unesite mail: ");
+		String mail = scanner.nextLine();
+		
+		System.out.println("Unesite svoju poziciju:");
+		int pozicija = Integer.parseInt(scanner.nextLine());
+		
+		scanner.close();
+       
+		MetodeJdbc.ubaciUTabeluEntuzijasti(ime, prezime, mail, pozicija);
+		*/
+		
+		
+		
+	}
+
+}
